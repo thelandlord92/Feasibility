7c3ae11e2af1e0237dc41a5fdea53ea889d8468c
thelandlord92 <bayo@onile.ai>
2024-05-28 18:13:58 +0200

    Testing common class.

---
+++ src/zero_touch/feasibility.common/common.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.DesignScript.Geometry;
+using ProtoCore.DSASM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feasibility.common
+{
+    /// <summary>
+    /// Wrapper class for the common elements.
+    /// </summary>
+    public class common
+    {
+        private common() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns point="Point"></returns>
+        public static Autodesk.DesignScript.Geometry.Point CreatePoint(int x, int y, int z)
+        {
+            var point = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x, y, z) as Autodesk.DesignScript.Geometry.Point;
+
+            return point;
+        }
+    }
+}
