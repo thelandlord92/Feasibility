6f546f02d7377a0460d01636f79417797d10c99a
thelandlord92 <bayo@onile.ai>
2024-06-01 17:29:05 +0200

    Multiple output tested.

--- src/zero_touch/Feasibility/Common/Common.cs
+++ src/zero_touch/Feasibility/Common/Common.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Feasibility.Common
+namespace Common
 {
-    internal class Common
+    /// <summary>
+    /// A class containing methods for common math operations.
+    /// </summary>
+    public class Math
     {
+        // hides the overall class as a node.
+        private Math() { }
+
+        /// <summary>
+        /// Add two integer values.
+        /// </summary>
+        /// <param name="a">the first integer</param>
+        /// <param name="b">the second integer</param>
+        /// <returns name="value">the added value</returns>
+        public static int IntAdd(int a, int b) 
+        {
+            // returns the addition of two integers.
+            return a + b; 
+        }
     }
 }
--- src/zero_touch/Feasibility/Parking/Parking.cs
+++ src/zero_touch/Feasibility/Parking/Parking.cs
@@ -4,18 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Feasibility.Parking
+namespace Parking
 {
-    public class Parking
+    /// <summary>
+    /// Wrapper class for the parking patterns.
+    /// </summary>
+    public class Patterns
     {
         // this hides the overall class as a node.
-        private Parking() { }
+        private Patterns() { }
 
-        public static Autodesk.DesignScript.Geometry.Point CreatePoint(int x, int y)
+        /// <summary>
+        /// Creates a point.
+        /// </summary>
+        /// <param name="x">the x coordinate value</param>
+        /// <param name="y">the y coordinate value</param>
+        /// <returns name="Point">the created point</returns>
+        /// <returns name="Number">the random number</returns>
+        public static Dictionary<string, object> CreatePoint(int x, int y)
         {
             var point = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x, y);
 
-            return point;
+            var intNumber = 25;
+
+            // Create a dictionary to hold the outputs.
+            var outputs = new Dictionary<string, object>();
+
+            // Add the various outputs to the dictionary.
+            outputs["Point"] = point as Autodesk.DesignScript.Geometry.Point;
+            outputs["Number"] = intNumber;
+
+            return outputs;
         }
     }
 }
