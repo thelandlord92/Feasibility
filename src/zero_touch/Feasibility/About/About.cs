2f00b79e6424be46e3d6ffc9558daf6adb535dc3
thelandlord92 <bayo@onile.ai>
2024-06-15 14:26:26 +0200

    Completed the about class.

--- src/zero_touch/Feasibility/About/About.cs
+++ src/zero_touch/Feasibility/About/About.cs
@@ -14,6 +14,21 @@ namespace About
         // this hides the overall class as a node.
         private About() { }
 
+        
+        /// <summary>
+        /// Capitalizes the first letter of a string.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns></returns>
+        private static string CapitalizeFirstLetter(string input) 
+        {
+            if (string.IsNullOrEmpty(input)) 
+            { 
+                return input;
+            } 
+            return $"{char.ToUpper(input[0])}{input.Substring(1)}";
+        }
+
 
         /// <summary>
         /// Provides information about the Feasibility plugin for Dynamo.
@@ -21,8 +36,10 @@ namespace About
         /// <returns></returns>
         public static string AboutFeasibility() 
         { 
-            string userName = Environment.UserName;
+            // get the username.
+            string userName = CapitalizeFirstLetter(Environment.UserName);
 
+            // create the about information.
             string aboutFeasibility = $"Hello {userName}, Feasibility is designed as a flexible toolkit to automate " +
                 $"various aspects of urban layout design using Dynamo ZeroTouch nodes. Leveraging the power of Dynamo, " +
                 $"this solution provides an efficient and scalable way to design and manipulate parking layouts, erven " +
