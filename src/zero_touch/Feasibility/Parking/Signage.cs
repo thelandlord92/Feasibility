0c6a9f5e9c4539e4de222a81f7c16e91333ffd62
thelandlord92 <bayo@onile.ai>
2024-06-15 15:20:54 +0200

    Added enums for signage type.

--- src/zero_touch/Feasibility/Parking/Signage.cs
+++ src/zero_touch/Feasibility/Parking/Signage.cs
@@ -12,6 +12,48 @@ using System.Threading.Tasks;
 namespace Parking
 {
     /// <summary>
+    /// Defines enums for the types of parking signage.
+    /// </summary>
+    public enum SignageType 
+    {
+        /// <summary>
+        /// Electric vehicle parking.
+        /// </summary>
+        EV,
+
+        /// <summary>
+        /// Parking for Persons With Disabilities.
+        /// </summary>
+        PWD,
+
+        /// <summary>
+        /// Generic parking P signage.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        /// Car parking.
+        /// </summary>
+        Car,
+
+        /// <summary>
+        /// Bicycle parking.
+        /// </summary>
+        Bicycle,
+
+        /// <summary>
+        /// Motorbike parking.
+        /// </summary>
+        Motorbike,
+
+        /// <summary>
+        /// No parking signage.
+        /// </summary>
+        NoParking,
+    }
+
+
+    /// <summary>
     /// Wrapper class for the parking signage.
     /// </summary>
     public class Signage
