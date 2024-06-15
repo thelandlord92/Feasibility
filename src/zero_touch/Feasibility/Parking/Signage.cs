d5bbe348648579134bb28dc3a5cf7ff7ded60b9a
thelandlord92 <bayo@onile.ai>
2024-06-15 16:57:06 +0200

    Moves the signage enums to independent file.

--- src/zero_touch/Feasibility/Parking/Signage.cs
+++ src/zero_touch/Feasibility/Parking/Signage.cs
@@ -12,53 +12,6 @@ using System.Threading.Tasks;
 namespace Parking
 {
     /// <summary>
-    /// Defines enums for the types of parking signage.
-    /// </summary>
-    public enum SignageType 
-    {
-        /// <summary>
-        /// Electric vehicle parking.
-        /// </summary>
-        EV,
-
-        /// <summary>
-        /// Parking for Persons With Disabilities.
-        /// </summary>
-        PWD,
-
-        /// <summary>
-        /// Generic parking P signage.
-        /// </summary>
-        Standard,
-
-        /// <summary>
-        /// Car parking.
-        /// </summary>
-        Car,
-
-        /// <summary>
-        /// Bicycle parking.
-        /// </summary>
-        Bicycle,
-
-        /// <summary>
-        /// Motorbike parking.
-        /// </summary>
-        Motorbike,
-
-        /// <summary>
-        /// No parking signage.
-        /// </summary>
-        NoParking,
-
-        /// <summary>
-        /// No signage requirement for the parking bay.
-        /// </summary>
-        NoSignage,
-    }
-
-
-    /// <summary>
     /// Wrapper class for the parking signage.
     /// </summary>
     public class Signage
---
+++ src/zero_touch/Feasibility/Parking/SignageTypes.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Parking
+{
+    /// <summary>
+    /// Defines enums for the types of parking signage.
+    /// </summary>
+    public enum SignageType
+    {
+        /// <summary>
+        /// Electric vehicle parking.
+        /// </summary>
+        EV,
+
+        /// <summary>
+        /// Parking for Persons With Disabilities.
+        /// </summary>
+        PWD,
+
+        /// <summary>
+        /// Generic parking P signage.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        /// Car parking.
+        /// </summary>
+        Car,
+
+        /// <summary>
+        /// Bicycle parking.
+        /// </summary>
+        Bicycle,
+
+        /// <summary>
+        /// Motorbike parking.
+        /// </summary>
+        Motorbike,
+
+        /// <summary>
+        /// No parking signage.
+        /// </summary>
+        NoParking,
+
+        /// <summary>
+        /// No signage requirement for the parking bay.
+        /// </summary>
+        NoSignage,
+    }
+}
