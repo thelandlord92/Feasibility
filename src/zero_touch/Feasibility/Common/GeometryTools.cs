e0477583b73b7116f44671af1fa27ebd7594c382
thelandlord92 <bayo@onile.ai>
2024-08-25 20:39:41 +0200

    Completed  the DiagonalHatchPattern() method.

--- src/zero_touch/Feasibility/Common/GeometryTools.cs
+++ src/zero_touch/Feasibility/Common/GeometryTools.cs
@@ -117,6 +117,7 @@ namespace Common
 
         /// <summary>
         /// Check the planarity of an input surface. Returns an error if the surface is not planar.
+        /// If planar it returns the surface.
         /// </summary>
         /// <param name="surface">The input surface.</param>
         /// <returns name="planarSurface">The planar surface.</returns>
@@ -136,6 +137,31 @@ namespace Common
             _surface = surface;
 
             return _surface;
+        }
+
+
+        /// <summary>
+        /// Check the planarity of an input curve. Returns an error if the curve.
+        /// If planar it returns the curve.
+        /// </summary>
+        /// <param name="curve">The input curve.</param>
+        /// <returns name="planarCurve">The planar curve.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static Curve CheckCurvePlanarity(Curve curve)
+        {
+            // get the max and min point of the curve bounding box.
+            Point minPoint = curve.BoundingBox.MinPoint;
+            Point maxPoint = curve.BoundingBox.MaxPoint;
+
+            // check if the surface is planar and horizontal.
+            Curve _curve = null;
+            if (maxPoint.Z > minPoint.Z)
+            {
+                throw new ArgumentException("The curve must be horizontal and planar.");
+            }
+            _curve = curve;
+
+            return _curve;
         }
 
 
@@ -499,7 +525,7 @@ namespace Common
             float dashGap = 2,
             float dashThickness = 2) 
         {
-            // Throw excpetion if inputs less than 1.
+            // Throw excpetion if inputs less than 0.001.
             if (dashLength < 0.001 || dashGap < 0.001 || dashThickness < 0.001) 
             {
                 throw new ArgumentException("dash length, gap, and thickness cannot be less than 0.001");
@@ -575,6 +601,100 @@ namespace Common
                 { "dashCenterCurves", dashCenterCurves },
                 { "dashOutlines", dashOutlines },
                 { "dashSurfaces", dashSurfaces }
+            };
+        }
+
+
+        /// <summary>
+        /// To create a diagonal hatch pattern.
+        /// </summary>
+        /// <param name="curve">The curve within which to define the hatch pattern.</param>
+        /// <param name="borderThickness">The thickness of the pattern's border.</param>
+        /// <param name="hatchThickness">The thickness of the diagonal hatches.</param>
+        /// <param name="hatchSpacing">The spacing between the hatches.</param>
+        /// <param name="hatchRotation">The rotaton of the hatch pattern.</param>
+        /// <returns name="hatchSurface">The surface of the hatch.</returns>
+        /// <returns name="hatchOutlines">The perimeter curves of the hatch surface.</returns>
+        /// <exception cref="Exception"></exception>
+        [MultiReturn(new[] { "hatchSurface", "hatchOutlines"})]
+        public static Dictionary<string, object> DiagonalHatchPattern(
+            Curve curve,
+            float borderThickness = 1f,
+            float hatchThickness = 1f,
+            float hatchSpacing = 5f,
+            float hatchRotation = 45f) 
+        {
+            // Throw excpetion if inputs less than 0.001.
+            if (borderThickness < 0.001 || hatchThickness < 0.001 || hatchSpacing < 0.001)
+            {
+                throw new ArgumentException("borderThickness, hatchThickness, and hatchSpacing cannot be less than 0.001");
+            }
+
+            // Check if the input curve is closed.
+            if (curve.IsClosed == false) 
+            {
+                throw new ArgumentException("The input curve must be closed");
+            }
+
+            // Check the planarity of the input polycuve.
+            Curve curve1 = CheckCurvePlanarity(curve);
+
+            // Create a surface from the curve.
+            Surface surface = Surface.ByPatch(curve1);
+
+            // Add the perimeter surface.
+            Surface perimeterSurface = PerimeterSurface(surface, borderThickness) as Surface;
+
+            // Create the bounding rectangle.
+            Rectangle boundingRectangle = CreateBoundingRectangle(curve1 as PolyCurve, hatchRotation);
+
+            // Create the hatch setout lines.
+            List<Line> lines = SetOutLines(boundingRectangle, hatchSpacing, hatchSpacing);
+
+            // Create the hatch outlines.
+            List<PolyCurve> hatchOutlines = new List<PolyCurve>();
+            foreach (Line line in lines)
+            {
+                hatchOutlines.Add(PolyCurve.ByThickeningCurveNormal(line, hatchThickness, Vector.ZAxis()));
+            }
+
+            // Create surfaces from the hatch outlines.
+            List<Surface> hatchSurfaces = new List<Surface>();
+            foreach (PolyCurve hatchOutline in hatchOutlines)
+            { 
+                hatchSurfaces.Add(Surface.ByPatch(hatchOutline));
+            }
+
+            // Intersect the hatch surfaces with hatch area surface.
+            List<Surface> intersectedSurfaces = new List<Surface>();
+            foreach (Surface hatchSurface in hatchSurfaces) 
+            {
+                // Cast the overall and hatch surfaces as geometry.
+                Geometry castSurface = surface as Geometry;
+                Geometry castHatchSurface = hatchSurface as Geometry;
+
+                // Intersect the overall and hatch surfaces.
+                Geometry[] surfaceList = castSurface.Intersect(castHatchSurface);
+
+                foreach (Geometry geometry in surfaceList) 
+                { 
+                    intersectedSurfaces.Add((Surface)geometry);
+                }
+            }
+
+            // Join the perimeter surface and the intersected hatch surfaces.
+            List<Surface> combinedSurfaces = new List<Surface>();
+            combinedSurfaces.AddRange(new List<Surface> { perimeterSurface });
+            combinedSurfaces.AddRange(intersectedSurfaces);
+            Surface joinedSurface = Surface.ByUnion(combinedSurfaces);
+
+            // Get the perimeter curves of the joined surface.
+            Curve[] perimeterCurves = joinedSurface.PerimeterCurves();
+
+            return new Dictionary<string, object>
+            {
+                { "hatchSurface", combinedSurfaces },
+                { "hatchOutlines", perimeterCurves }
             };
         }
     }
